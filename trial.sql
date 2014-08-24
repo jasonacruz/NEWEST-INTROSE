231d3d81224c087ef16433830a8d73723334cf13
jasonacruz <jasoon.cruz@yahoo.com>
2014-08-24 21:32:18 +0800

    updatse

--- trial.sql
+++ trial.sql
@@ -1,11 +1,17 @@
-INSERT INTO EMPLOYEE(`idEmployee`, `firstNameEmp`, `middleNameEmp`, `lastNameEmp`,  `contactNumberEmp`, `positionEmp`, `departmentEmp`, `passwordEmp`, `addressEmp`, `passFlagEmp`) VALUES (99999999, '','','','','Administrator', '','admin1234', '', 1);
+INSERT INTO EMPLOYEE(idEmployee,firstNameEmp,middleNameEmp,lastNameEmp,contactNumberEmp,positionEmp,departmentEmp,passwordEmp,addressEmp,passFlagEmp) VALUES (99999999, '','','','','Administrator', '','admin1234', '', 1);
 
-INSERT INTO EMPLOYEE(`idEmployee`, `firstNameEmp`, `middleNameEmp`, `lastNameEmp`,  `contactNumberEmp`, `positionEmp`, `departmentEmp`, `passwordEmp`, `addressEmp`, `passFlagEmp`) VALUES (11233877, 'Jason','Anthony','Cruz','1234567','Subject Teacher', 'Elementary','Isawboy', 'blabla', 1);
+INSERT INTO EMPLOYEE(idEmployee,firstNameEmp,middleNameEmp,lastNameEmp,contactNumberEmp,positionEmp,departmentEmp,passwordEmp,addressEmp,passFlagEmp) VALUES (11233877, 'Jason','Anthony','Cruz','1234567','Subject Teacher', 'Elementary','Isawboy', 'blabla', 1);
 
-INSERT INTO EMPLOYEE(`idEmployee`, `firstNameEmp`, `middleNameEmp`, `lastNameEmp`,  `contactNumberEmp`, `positionEmp`, `departmentEmp`, `passwordEmp`, `addressEmp`, `passFlagEmp`) VALUES (12345678, 'Jason','Anthon','Cruz','1234567','Principal', 'NonE','Isawboy', 'blabla', 1);
+INSERT INTO EMPLOYEE(idEmployee,firstNameEmp,middleNameEmp,lastNameEmp,contactNumberEmp,positionEmp,departmentEmp,passwordEmp,addressEmp,passFlagEmp) VALUES (12345678, 'Jason','Anthon','Cruz','1234567','Principal', 'NonE','Isawboy', 'blabla', 1);
 
 INSERT INTO SECTION(`sectionName`, `sectionYear`) VALUES ('Peace', 2);
-INSERT INTO STUDENT(idStudent,firstNameSt,middleNameSt,lastNameSt,genderSt,birthDateSt,yearLevelSt) VALUES (123456, 'jason', 'antyhon', 'cruz', 'm', 'October-12-1995', '1'); 
-INSERT INTO CLASS(`Section_idSection`, `classYear`, `studentID`, `adviserID`) VALUES(1, 2014, 0,0);
+INSERT INTO SECTION(`sectionName`, `sectionYear`) VALUES ('Love', 5);
 
-SELECT  CURRENT_DATE() as YEARNOW, date_format(CURRENT_DATE(), '%M %d, %Y');
+INSERT INTO STUDENT(idStudent,firstNameSt,middleNameSt,lastNameSt,genderSt,birthDateSt,yearLevelSt) VALUES (123456, 'jason', 'antyhon', 'cruz', 'm', 'October-12-1995', '1');
+INSERT INTO STUDENT(idStudent,firstNameSt,middleNameSt,lastNameSt,genderSt,birthDateSt,yearLevelSt) VALUES (1234567, 'jasone', 'antyhon', 'cruz', 'm', 'October-12-1995', '1'); 
+INSERT INTO STUDENT(idStudent,firstNameSt,middleNameSt,lastNameSt,genderSt,birthDateSt,yearLevelSt) VALUES (12345678, 'jasonee', 'antyhon', 'cruz', 'm', 'October-12-1995', '1'); 
+INSERT INTO CLASSADVISER(classYear,classSection,adviserID) VALUES(2013, 'Peace',99999999);
+INSERT INTO CLASSADVISER(classYear,classSection,adviserID) VALUES(2014, 'Love',12345678);
+INSERT INTO CLASSADVISER(classYear,classSection,adviserID) VALUES(2015, 'Peace',99999999);
+INSERT INTO CLASSADVISER(classYear,classSection,adviserID) VALUES(2014, 'Peace',11233877);
+INSERT INTO STUDENTROSTER(sr_ClassID,sr_StudentID) VALUES(1, 123456);
